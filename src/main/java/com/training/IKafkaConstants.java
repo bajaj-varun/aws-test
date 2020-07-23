08cdaf7e26e8628cbbbf210bbca006a6b4b78a3f
Varun Bajaj <bajaj.varun@hotmail.com>
2020-07-24 00:34:07 +0530

    Update IKafkaConstants.java

--- src/main/java/com/training/IKafkaConstants.java
+++ src/main/java/com/training/IKafkaConstants.java
@@ -6,7 +6,7 @@ import org.apache.kafka.common.serialization.StringDeserializer;
 import org.apache.kafka.common.serialization.StringSerializer;
 
 public interface IKafkaConstants {
-    public String BOOTSTRAP_SERVERS="13.235.90.108:9092";
+    public String BOOTSTRAP_SERVERS="";
     public String T0_TOPIC = "T0";
 
     public Class<StringDeserializer> stringDeserializer = StringDeserializer.class;
